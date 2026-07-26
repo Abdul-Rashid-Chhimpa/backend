2b8445cb3c491fab9817051faec7283a7e89e0cb
Abdul Rashid Chhimpa <110084078+Abdul-Rashid-Chhimpa@users.noreply.github.com>
2026-07-26 07:57:55 -0700

    Update authRoutes.js

--- Routers/authRoutes.js
+++ Routers/authRoutes.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const {
   register,
   login,
-} = require("../Controllers/authController");
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/authController"); // path check kar lena
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 module.exports = router;
