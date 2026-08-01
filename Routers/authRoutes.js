4fc828c874599e3953d1f73b410b2bef7ad55900
Abdul Rashid Chhimpa <110084078+Abdul-Rashid-Chhimpa@users.noreply.github.com>
2026-08-01 13:38:35 -0700

    Update authRoutes.js

--- Routers/authRoutes.js
+++ Routers/authRoutes.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
-
+import { protect } from "../middleware/authMiddleware.js";
 const {
   register,
   login,
   forgotPassword,
   resetPassword,
+   updateProfile
 } = require("../Controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
+router.put("/profile", protect, updateProfile);
 
 module.exports = router;
