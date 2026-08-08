902ed052d6a0f0703a5eca2cc6b993fc023c080a
Abdul Rashid Chhimpa <110084078+Abdul-Rashid-Chhimpa@users.noreply.github.com>
2026-08-08 12:18:00 -0700

    Add duplicate userRoutes endpoint for '/api/users'

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api", userRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api", settingsRoutes);
 app.use("/api", categoryRoutes);
 
